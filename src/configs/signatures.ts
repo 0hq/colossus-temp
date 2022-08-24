bbe5b5e1d353d7020b546db6fdd39d34a7ee65c5
Christian Glassiognon <christian@heyglassy.co>
2022-08-24 03:41:32 -0700

    remove shuffle function (unused)

--- src/configs/signatures.ts
+++ src/configs/signatures.ts
@@ -48,7 +48,7 @@ import SauravPicture from "../../public/profile-pictures/Saurav.jpg";
 
 import { StaticImageData } from "next/image";
 
-type Signatures = {
+export type Signatures = {
   image: StaticImageData;
   name: string;
   subject: string;
@@ -57,26 +57,6 @@ type Signatures = {
   signature?: any;
   gold: boolean;
 };
-
-function shuffle(array: any[]) {
-  let currentIndex = array.length,
-    randomIndex;
-
-  // While there remain elements to shuffle.
-  while (currentIndex != 0) {
-    // Pick a remaining element.
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex],
-      array[currentIndex],
-    ];
-  }
-
-  return array;
-}
 
 export const signatures: Array<Signatures> = [
   {
