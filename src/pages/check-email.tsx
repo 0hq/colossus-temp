f5cf6935ad8da61a3534e94f733eabe34f15a743
Christian Glassiognon <christian@heyglassy.co>
2022-08-24 03:42:52 -0700

    Updated check email with new header

--- src/pages/check-email.tsx
+++ src/pages/check-email.tsx
@@ -1,17 +1,18 @@
 import type { NextPage } from "next";
-import { Main, Container, TextWrapper, Footer } from "../components/basics";
+import { Main, Container, TextWrapper } from "../components/basics";
 import Header from "src/components/public-header";
 
 const CheckEmail: NextPage = () => {
   return (
     <Main>
-      <Header link="" button="Join the Colossus" />
-      <Container title="/ Check your email!" infotext="Resend email">
+      <Header link="https://twitter.com/colossusfyi" value="Need Help?" />
+      <Container title="/ Check your email!" link="Resend email">
         <TextWrapper>
           <h1>We just sent you a magic login link to your email,</h1>
           <br />
           <h1>
-            Please check your <span className="font-bold">spam</span> folder if you don’t see it in your inbox.
+            Please check your <span className="font-bold">spam</span> folder if
+            you don’t see it in your inbox.
           </h1>
         </TextWrapper>
       </Container>
